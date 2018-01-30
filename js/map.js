496b26df248a517474a617f0c0be04a9f0c8fa09
Liubov Alifanova <lunay17@bk.ru>
2018-01-30 23:35:17 +0300

    commit

---
+++ js/map.js
@@ -0,0 +1,25 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.938613982849, 30.322900507585],
+            zoom: 15
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/pin.png',
+            // Размеры метки.
+            iconImageSize: [218, 142],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-3, -42]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+});
